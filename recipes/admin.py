c16927bb272da0d90b355080bf33f2dc3d252e44
Thiago Freitas <tfreitasdevv@gmail.com>
2023-03-22 18:02:04 -0300

    Criando Model Recipe

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Recipe
 
 # Register your models here.
 
@@ -8,4 +8,10 @@ class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
+@admin.register(Recipe)  # forma 1 de registrar, com o anotator @admin.register
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+# forma 2 de registrar, com a função register
 admin.site.register(Category, CategoryAdmin)
